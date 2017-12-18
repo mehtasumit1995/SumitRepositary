ed71ce15a1ee9e6651889d11997a45586cb3f30e
Mehta <sumit.c.mehta@capgemini.com>
2017-12-18 20:14:06 +0530

    First commit

---
+++ src/main/java/com/cg/test/SpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.cg.test;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+
+}
